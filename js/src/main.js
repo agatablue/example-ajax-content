dab9332fb9b77c084b944d1b97a0eef29005a402
agata <agatablue@gmail.com>
2015-07-20 21:33:03 +0200

    Initial version

--- js/src/main.js
+++ js/src/main.js
@@ -1,8 +1,11 @@
-(function (global) {
+$(function () {
 
     'use strict';
 
-
+    /* Get command object
+       @param object data (with action_id)
+       @return deferred object
+     */
     function getCommand(data) {
         return $.ajax({
             url: 'php/index.php',
@@ -11,9 +14,11 @@
         });
     }
 
-
+    /* Render view Class
+        @param object data
+        @return function handleData
+     */
     var ViewRenderer = function (data) {
-
         var $mainElement = $('#main-contener');
         var triggerCounter = 0;
         var styleType = {
@@ -22,39 +27,30 @@
             three: 'three'
         };
 
+        /* Change class
+            @param string className
+         */
         function swapStyle(className) {
             $mainElement.removeClass().addClass(className);
         }
 
+        /* Reset view
+         */
         function resetView() {
             $mainElement.empty();
         }
 
-
-        function showView(responseData) {
-            var response = JSON.parse(responseData);
-            switch (response.type) {
-                case 'display':
-                {
-                    display(response.data);
-                    break;
-                }
-                case 'trigger':
-                {
-                    trigger(response.data);
-                    break;
-                }
-                default: break;
-            }
-        }
-
-        function display(data) {
+        /* Render template using Handlebars
+          TODO: Create a template converter
+          @param object data
+         */
+        function renderView(data) {
             resetView();
             var rootKeys = _.keys(data);
 
             var source = $('#entry-template').html();
             var template = Handlebars.compile(source);
-            var context = {
+            var content = {
                'divFirst': {
                     'className': rootKeys[0].substring(rootKeys[0].indexOf('.') + 1),
                     'h1_text_contents': data['div.first'].h1.text_contents,
@@ -66,10 +62,12 @@
                     'span_text_contents': data['div.second'].span.span.text_contents
                 }
             };
-            $mainElement.append(template(context));
+            $mainElement.append(template(content));
         }
 
-        function trigger(data) {
+        /* According to triggerCounter swap class for content
+         */
+        function trigger() {
             switch (triggerCounter % 3) {
                 case 0:
                 {
@@ -90,26 +88,60 @@
             triggerCounter++;
         }
 
+
+        /* Manage view according to type of response object
+         @param string responseData
+         */
+        function handleData(responseData) {
+            if (responseData) {
+                try {
+                    var response = JSON.parse(responseData);
+                    switch (response.type) {
+                        case 'display':
+                        {
+                            renderView(response.data);
+                            break;
+                        }
+                        case 'trigger':
+                        {
+                            trigger();
+                            break;
+                        }
+                        default:
+                            break;
+                    }
+                } catch (error) {
+                    console.log(error);
+                }
+            }
+        }
+
         return {
-            showView: showView
+            handleData: handleData
         };
     };
 
-
-    var ManagerView = function () {
+    /* Controller Class to get data from server
+      @return function init
+     */
+    var Controller = function () {
         var data = {};
         var viewRenderer = new ViewRenderer();
 
         function init() {
-            for (var i = 1, j = 0; i <= 9000; i++, j++) {
+            for (var i = 1; i <= 9000; i++) {
                 setTimeout(function (counter) {
                     return function () {
                         data.action_id = counter;
-                        getCommand(data).done(function (responseData) {
-                            viewRenderer.showView(responseData);
-                        });
+                        getCommand(data)
+                                        .done(function (responseData) {
+                                            viewRenderer.handleData(responseData);
+                                        })
+                                        .fail(function(jqXHR, textStatus, errorThrown) {
+                                            throw new Error(errorThrown);
+                                        });
                     };
-                }(j), 1000 * i);
+                }(i), 1000 * i);
             }
         }
 
@@ -119,7 +151,7 @@
         };
     };
 
-    var managerView = new ManagerView();
-    managerView.init();
+    var controller = new Controller();
+    controller.init();
 
-}(this));
+}());
